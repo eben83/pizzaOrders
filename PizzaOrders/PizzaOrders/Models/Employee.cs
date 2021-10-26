25ad98fda0c4297d17be3e0ac4319339f8509606
Eben Burger <eben@arlowconsulting.co.za>
2021-10-26 15:15:51 +0200

    added Employee Model

---
+++ PizzaOrders/PizzaOrders/Models/Employee.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace PizzaOrders.Models
+{
+    public class Employee
+    {
+        [Key]//primary key- attribute
+        public int Id { get; set; }
+
+        public string Name { get; set; }
+
+        public string Department { get; set; }
+    }
+}
