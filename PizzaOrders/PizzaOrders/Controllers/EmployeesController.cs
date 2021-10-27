38d6742f4432cab50bc086cbe3eaa098dfc69203
Eben Burger <eben@arlowconsulting.co.za>
2021-10-27 12:04:52 +0200

    added EmployeeController

---
+++ PizzaOrders/PizzaOrders/Controllers/EmployeesController.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace PizzaOrders.Controllers
+{
+    public class EmployeesController : Controller
+    {
+        // GET
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
