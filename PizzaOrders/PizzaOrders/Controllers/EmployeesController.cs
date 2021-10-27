24bfe4ba6f60d89dca2100b0236c180495b2c509
Eben Burger <eben@arlowconsulting.co.za>
2021-10-27 12:44:00 +0200

    added IEnum to Employee Controller

--- PizzaOrders/PizzaOrders/Controllers/EmployeesController.cs
+++ PizzaOrders/PizzaOrders/Controllers/EmployeesController.cs
@@ -1,13 +1,32 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using System.Collections;
+using System.Collections.Generic;
+using Microsoft.AspNetCore.Mvc;
+using PizzaOrders.Data;
+using PizzaOrders.Models;
 
 namespace PizzaOrders.Controllers
 {
     public class EmployeesController : Controller
     {
-        // GET
+        //prop 
+        private readonly ApplicationDbContext _db;
+
+        //ctor- to populate the above prop 
+        //this will pull from the DbContext container
+        //you can just add the Class or Interface as a parameter with a name
+        public EmployeesController(ApplicationDbContext db)
+        {
+            //local _db will be assigned to the DbContext
+            //(ApplicationDbContext db) object will have an instance of the DbContext
+            //_db will now be available throughout the controller
+            _db = db;
+        }
         public IActionResult Index()
         {
-            return View();
+            //making an IEnumerable to display the list of Employees
+            //
+            IEnumerable<Employees> objectList = _db.Employees;
+            return View(objectList);
         }
     }
 }
