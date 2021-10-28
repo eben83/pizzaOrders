1a7b432ca001a163eea1d856870026ff49ff97db
Eben Burger <eben@arlowconsulting.co.za>
2021-10-28 14:18:29 +0200

    order updated to singular name

---
+++ PizzaOrders/PizzaOrders/Controllers/OrderController.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using PizzaOrders.Data;
+using PizzaOrders.Models;
+
+namespace PizzaOrders.Controllers
+{
+    public class OrderController : Controller
+    {
+
+        private readonly ApplicationDbContext _db;
+
+        public OrderController(ApplicationDbContext db)
+        {
+            _db = db;
+        }
+        
+        
+        // Get - CREATE
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        //POST - CREATE
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Create(Order obj)
+        {
+            var employee = new Employee();
+            var order = new Order();
+            order.Employee = employee;
+            order.EmployeeId = employee.EmployeeId;
+            var employeeName = order.Employee.Name;
+            
+            _db.Add(obj);
+            _db.SaveChanges();
+            return View();
+        }
+    }
+}
--- PizzaOrders/PizzaOrders/Models/Order.cs
+++ PizzaOrders/PizzaOrders/Models/Order.cs
@@ -1,23 +1,30 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.ComponentModel;
 using System.ComponentModel.DataAnnotations;
 
 namespace PizzaOrders.Models
 {
-    public class Orders
+    public class Order
     {
         [Key]
         public int Id { get; set; }
 
+        [DisplayName("Date Time")]
         public DateTime DateTime { get; set; }
         
-        [DisplayName("Employee Name")]
-        public Employees Name { get; set; }
 
         public string Pizza { get; set; }
 
         public string Drink { get; set; }
         
-        
+        public int EmployeeId { get; set; }
+        public virtual Employee Employee { get; set; }
+
+        public void Test()
+        {
+            var employeeName = Employee.Name;
+            Employee = new Employee();
+        }
     }
 }
