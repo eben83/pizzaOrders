e24440d4bf83db7efcbbfa385ac672a8ada98b8f
Eben Burger <eben@arlowconsulting.co.za>
2021-10-28 11:27:28 +0200

    Orders migration

---
+++ PizzaOrders/PizzaOrders/Migrations/20211028092432_addOrdersToDatabse.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace PizzaOrders.Migrations
+{
+    public partial class addOrdersToDatabse : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Orders",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    DateTime = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    NameId = table.Column<int>(type: "int", nullable: true),
+                    Pizza = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Drink = table.Column<string>(type: "nvarchar(max)", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Orders", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Orders_Employees_NameId",
+                        column: x => x.NameId,
+                        principalTable: "Employees",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Restrict);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Orders_NameId",
+                table: "Orders",
+                column: "NameId");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Orders");
+        }
+    }
+}
