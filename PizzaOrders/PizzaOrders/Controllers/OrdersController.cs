f1c5a2ecf3d98725b308cba252a004660c88ebac
Eben Burger <eben@arlowconsulting.co.za>
2021-10-28 10:33:44 +0200

    orders Create added, model, control & View

---
+++ PizzaOrders/PizzaOrders/Controllers/OrdersController.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using PizzaOrders.Data;
+using PizzaOrders.Models;
+
+namespace PizzaOrders.Controllers
+{
+    public class OrdersController : Controller
+    {
+
+        private readonly ApplicationDbContext _db;
+
+        public OrdersController(ApplicationDbContext db)
+        {
+            _db = db;
+        }
+        // POST - CREATE
+        public IActionResult Create()
+        {
+            return View();
+        }
+    }
+}
---
+++ PizzaOrders/PizzaOrders/Models/Orders.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+
+namespace PizzaOrders.Models
+{
+    public class Orders
+    {
+        [Key]
+        public int Id { get; set; }
+
+        public DateTime DateTime { get; set; }
+        
+        [DisplayName("Employee Name")]
+        public Employees Name { get; set; }
+
+        public string Pizza { get; set; }
+
+        public string Drink { get; set; }
+        
+        
+    }
+}
